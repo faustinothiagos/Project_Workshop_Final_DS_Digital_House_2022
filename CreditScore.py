c41a6635ca718451b050dd3974a67e6110e53211
felipefl142 <felipefrl1@hotmail.com>
2022-10-19 12:43:33 -0300

    gg

--- CreditScore.py
+++ CreditScore.py
@@ -8,20 +8,20 @@ st.header('O que é Credit Score?')
 st.subheader('Credit Score é uma pontuação resultante dos hábitos de pagamento e relacionamento do consumidor com o mercado de crédito. Fatores como pagar contas em dia, histórico de dívidas negativadas, saldo devedor etc, representam o risco de inadimplência e saúde financeira geral de uma pessoa. Pode ser Ruim, Regular, Bom.')
 st.subheader('\n')
 st.subheader('\n')
+st.subheader('**ATENÇÃO: Este modelo não é o oficial e foi criado com a finalidade de ter uma versão interativa no StreamLit. Como as variáveis envolvidas não são tão fáceis de se obter/lembrar para inserir, foram escolhidas algumas delas com esse propósito.  A performance deste modelo é muito pior. Não é recomendado para uso comercial.')
 
 st.sidebar.subheader('Insira seus dados abaixo e descubra sua pontuação.')
 divida = st.sidebar.slider("Qual é a sua dívida?", 0,10000, step = 250)/2
 salario_mensal = st.sidebar.slider("Qual o seu salário mensal?", 0,25000, step = 500)/2
 investimento = st.sidebar.slider("Quanto você investe mensalmente?", 0,1000, step = 50)/2
 tempo_cartao = st.sidebar.slider("Quantos anos você possui o cartão?", 0,25)*12
-fatura = st.sidebar.radio('Você paga a fatura do cartão em dia?',('sim','não'))
-
+fatura = st.sidebar.radio('Você paga a fatura do cartão em dia?',('Sim','Não'))
 
 dados_dict = {'Outstanding_Debt':divida,
               'Monthly_Inhand_Salary':salario_mensal,
               'Credit_History_age':tempo_cartao,
               'Amount_invested_monthly':investimento,
-              'Payment_of_Min_Amount':fatura      
+              'Payment_of_Min_Amount':fatura
              }
 
 col = ['Outstanding_Debt','Monthly_Inhand_Salary','Credit_History_age','Amount_invested_monthly',
